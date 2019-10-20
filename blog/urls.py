c7d70e95c91957ff2c38bbb1cfbf7ae4617cf116
Maria <mariamarcosnunez@gmail.com>
2019-10-20 19:18:20 +0200

    Agregadas vistas y plantilla para el detalle del post del blog y CSS

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('W3', views.W3, name='W3'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
